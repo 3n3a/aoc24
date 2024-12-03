c5463e7c0505613f2c30150e834210d327c865d8
3n3a <46775561+3n3a@users.noreply.github.com>
2024-12-03 08:15:46 +0000

    add day2 part b

--- aoc/day2.py
+++ aoc/day2.py
@@ -1,14 +1,10 @@
+from itertools import combinations
 from aocd import get_data, submit
 
 day = 2
 year = 2024
+part = "a"
 data = get_data(day=day, year=year)
-# data = """7 6 4 2 1
-# 1 2 7 8 9
-# 9 7 6 2 1
-# 1 3 2 4 5
-# 8 6 4 4 1
-# 1 3 6 7 9"""
 
 def to_levels(report_line):
     levels = report_line.split(" ")
@@ -17,64 +13,49 @@ def to_levels(report_line):
 reports_lines = data.split("\n")
 reports = list(map(to_levels, reports_lines))
 
-# 1. all increasing / all decreasing
-# 2. 1, 2, 3 -> only acceptable steps
+inc = [1, 2, 3]
+dec = [-1, -2, -3]
 
-def get_report_directions(report):
-    """report direction (increasing, decreasing, unknown)
-    """
-    directions = [] # -1 = dec, 0 = none, 1 = inc
-    for i, level in enumerate(report):
-        if i+1 < len(report):
-            next = report[i+1]
-        else:
-            continue
-
-        # print(i, level, next, next > level)
-        if next > level:
-            directions.append(1)
-        elif next < level:
-            directions.append(-1)
-        elif next == level:
-            directions.append(0)
-    return directions
+def is_safe(levels):
+    # no inc / dec
+    if levels[0] == levels[1]:
+        return False
     
-def get_report_steps(report):
-    steps = []
-    for i, level in enumerate(report):
-        if i+1 < len(report):
-            next = report[i+1]
-        else:
-            continue
-        
-        steps.append(abs(next - level))
-    return steps
+    not_inc = False
+    not_dec = False
 
+    for i, level in enumerate(levels[1:]):
+        prev = levels[i]
+        if prev - level not in inc:
+            not_inc = True
+        if prev - level not in dec:
+            not_dec = True
 
-def is_report_safe(report):
-    directions = get_report_directions(report)
-    allInc = all(x == 1 for x in directions)
-    allDec = all(x == -1 for x in directions)
+    if not_inc and not_dec:
+        return False
+    else:
+        return True
     
-    steps = get_report_steps(report)
-    steps_within_limits = all(x >= 1 and x <= 3 for x in steps)
-    safe_direction = allInc or allDec
-    return safe_direction and steps_within_limits
+def calc_safe_count():
+    safe_count = 0
+    for report in reports:
+        if is_safe(report):
+            safe_count += 1
+        else:
+            if not part == "b":
+                continue
+            # bruteforce
+            level_subsets = combinations(report, len(report) - 1)
+            found_safe = False
+            for sub in level_subsets:
+                if is_safe(sub):
+                    found_safe = True
+            if found_safe:
+                safe_count += 1
 
-safe_reports_count = 0
-for i, report in enumerate(reports):
-    if is_report_safe(report):
-        safe_reports_count += 1
-        del reports[i]
+    return safe_count
 
+submit(calc_safe_count(), part=part, day=day, year=year)
 
-print("a", safe_reports_count, safe_reports_count == 2)
-#submit(safe_reports_count, part="a", day=day, year=year)
-
-# safe_reports_count2 = 0
-# for report in reports2:
-#     if is_report_safe(report):
-#         safe_reports_count2 += 1
-
-# print("b", safe_reports_count2, safe_reports_count2 == 4)
-# submit(safe_reports_count2, part="b", day=day, year=year)
+part = "b"
+submit(calc_safe_count(), part=part, day=day, year=year)
