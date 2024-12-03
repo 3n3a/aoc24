6cd60cdb0fbda84d3869591db8e0256276977343
3n3a <46775561+3n3a@users.noreply.github.com>
2024-12-03 06:11:03 +0000

    day3 part 2

--- aoc/day3.py
+++ aoc/day3.py
@@ -8,21 +8,29 @@ data = get_data(day=day, year=year)
 
 import re
 
-regex = r"mul\(([0-9]+),([0-9]+)\)"
+#regex = r"mul\(([0-9]+),([0-9]+)\)"
+regex = r"do\(\)|don\'t\(\)|mul\(([0-9]+),([0-9]+)\)"
 
 test_str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 
 matches = re.finditer(regex, data, re.MULTILINE)
 
 mult = []
+do_mult = True
 for matchNum, match in enumerate(matches, start=1):
     
     # mul(1,2)
-    groups = list(match.groups())
-    left = int(groups[0])
-    right = int(groups[1])
-    mult.append(left * right)
+    if "mul" in str(match) and do_mult:
+        groups = list(match.groups())
+        left = int(groups[0])
+        right = int(groups[1])
+        mult.append(left * right)
 
+    if "don" in str(match):
+        do_mult = False
+
+    elif "do" in str(match):
+        do_mult = True
     #print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
     
     #for groupNum in range(0, len(match.groups())):
@@ -34,4 +42,13 @@ for matchNum, match in enumerate(matches, start=1):
 
 answer = sum(mult)
 print(mult, answer)
-submit(answer, part="a", day=day, year=year)
+submit(answer, part="b", day=day, year=year)
+
+
+# do\(\)|don\'t\(\)|mul\(([0-9]+),([0-9]+)\)
+
+# each check if is "mul"
+# if do => do_multiply = True
+# if dont => do_multiply = False
+
+#same loop but with conditional multiply
