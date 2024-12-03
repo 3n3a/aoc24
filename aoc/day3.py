2d84698e4388b59939f1f1bbaa425c4d94a06a9f
3n3a <46775561+3n3a@users.noreply.github.com>
2024-12-03 06:45:42 +0000

    finish day 3 code cleanup

--- aoc/day3.py
+++ aoc/day3.py
@@ -3,52 +3,56 @@ from aocd import get_data, submit
 day = 3
 year = 2024
 data = get_data(day=day, year=year)
-# coding=utf8
-# the above tag defines encoding for this docume
 
 import re
 
-#regex = r"mul\(([0-9]+),([0-9]+)\)"
-regex = r"do\(\)|don\'t\(\)|mul\(([0-9]+),([0-9]+)\)"
 
-test_str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+def part_a(data):
 
-matches = re.finditer(regex, data, re.MULTILINE)
+    regex = r"mul\(([0-9]+),([0-9]+)\)"
 
-mult = []
-do_mult = True
-for matchNum, match in enumerate(matches, start=1):
-    
-    # mul(1,2)
-    if "mul" in str(match) and do_mult:
+    # example data
+    # test_str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+
+    matches = re.finditer(regex, data, re.MULTILINE)
+
+    mult = []
+    do_mult = True
+    for matchNum, match in enumerate(matches, start=1):
         groups = list(match.groups())
         left = int(groups[0])
         right = int(groups[1])
         mult.append(left * right)
 
-    if "don" in str(match):
-        do_mult = False
+    answer = sum(mult)
+    print(mult, answer)
+    submit(answer, part="a", day=day, year=year)
 
-    elif "do" in str(match):
-        do_mult = True
-    #print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
-    
-    #for groupNum in range(0, len(match.groups())):
-     #   groupNum = groupNum + 1
-      # 
-    #    print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
+def part_b(data):
+    regex = r"do\(\)|don\'t\(\)|mul\(([0-9]+),([0-9]+)\)"
 
-# Note: for Python 2.7 compatibility, use ur"" to prefix the regex and u"" to prefix the test string and substitution.
+    matches = re.finditer(regex, data, re.MULTILINE)
 
-answer = sum(mult)
-print(mult, answer)
-submit(answer, part="b", day=day, year=year)
+    mult = []
+    do_mult = True
+    for matchNum, match in enumerate(matches, start=1):
+        
+        # mul(1,2)
+        if "mul" in str(match) and do_mult:
+            groups = list(match.groups())
+            left = int(groups[0])
+            right = int(groups[1])
+            mult.append(left * right)
 
+        if "don" in str(match):
+            do_mult = False
 
-# do\(\)|don\'t\(\)|mul\(([0-9]+),([0-9]+)\)
+        elif "do" in str(match):
+            do_mult = True
 
-# each check if is "mul"
-# if do => do_multiply = True
-# if dont => do_multiply = False
+    answer = sum(mult)
+    print(mult, answer)
+    submit(answer, part="b", day=day, year=year)
 
-#same loop but with conditional multiply
+part_a(data)
+part_b(data)
