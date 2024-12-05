584ebe4a77b7d352e0f63d63a41e87e267aaf262
3n3a <46775561+3n3a@users.noreply.github.com>
2024-12-05 07:46:09 +0000

    day5 part b

--- aoc/day5.py
+++ aoc/day5.py
@@ -16,21 +16,74 @@ rules = list(map(rule_split, sections[0].split("\n")))
 pages = list(map(page_split, sections[1].split("\n")))
 answer = 0
 
-# compare each rule to each page list
-# if rule numbers are contained withing target
-for page in pages:
+def is_safe(page, return_bad_indexes = False):
     is_safe = []
+    bad_indexes = []
     for rule in rules:
         if rule[0] in page and rule[1] in page:
             # check if rule correctly applied
             v0 = page.index(rule[0])
             v1 = page.index(rule[1])
 
-            is_safe.append(v0 < v1)
+            cur_safe = v0 < v1
+            is_safe.append(cur_safe)
+
+            if cur_safe:
+                bad_indexes.append(None)
+            else:
+                bad_indexes.append([v0, v1])
     
-    if all(is_safe):
+    if return_bad_indexes:
+        return all(is_safe), bad_indexes
+
+    return all(is_safe)
+
+bad_pages = []
+
+# compare each rule to each page list
+# if rule numbers are contained withing target
+for page in pages:    
+    if is_safe(page):
         middleIndex = math.ceil(len(page) / 2) - 1
         answer += int(page[middleIndex])
+    else:
+        bad_pages.append(page)
 
 print(answer)
 submit(answer, part="a", day=day, year=year)
+
+# part b
+answer2 = 0
+
+def find_bad_page_ordering(page, rules):
+    for li, ri in rules:
+        try:
+            lidx, ridx = page.index(li), page.index(ri)
+        except ValueError:
+            # Atleast one of the values of this rule is not
+            # in the update -- this rule is not violated.
+            continue
+        if lidx > ridx:
+            # Update does not follow order -- returning the errant indices.
+            return (lidx, ridx)
+    # Update follows all ordering rules.
+    return None
+
+def find_correct_page_order(page, rules):
+    while (result := find_bad_page_ordering(page, rules)) is not None:
+        # Swap the bad items and try again.
+        lidx, ridx = result
+        page[lidx], page[ridx] = page[ridx], page[lidx]
+    return page
+
+
+fixes_pages = (
+    find_correct_page_order(u, rules)
+    for u in pages
+    if find_bad_page_ordering(u, rules) is not None
+)
+middle_values = (int(u[len(u) // 2]) for u in fixes_pages)
+answer2 = sum(middle_values)
+
+print(answer2)
+submit(answer2, part="b", day=day, year=year)
